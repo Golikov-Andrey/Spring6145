0d711be96d636fd975825b87a8a9d8bbfd544d9f
sergey <saizotov81@gmail.com>
2024-04-09 11:46:38 +0500

    добавил методы удаления user

--- Seminar02/my_first_crud_no_CSS/empl4sem2CRUD/src/main/java/com/example/empl4sem2CRUD/repositories/UserRepository.java
+++ Seminar02/my_first_crud_no_CSS/empl4sem2CRUD/src/main/java/com/example/empl4sem2CRUD/repositories/UserRepository.java
@@ -38,6 +38,8 @@ public class UserRepository {
         return  user;
     }
 
-    //public void deleteById(int id)
-    //"DELETE FROM userTable WHERE id=?"
+    public void deleteById(int id) {
+        String sql = "DELETE FROM userTable WHERE id=?";
+        jdbc.update(sql, id);
+    }
 }
