73c824a7efcab224f49dfab42dfe61a9d1f55190
Golikov-Andrey <newsc2@yandex.ru>
2024-05-14 20:59:55 +0300

    Solution from seminar 10 tests

---
+++ Seminar10/example1_sem10/sq-ch14-ex1/src/main/java/com/example/dto/TransferRequest.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransferRequest {
+
+  private long senderAccountId;
+  private long receiverAccountId;
+  private BigDecimal amount;
+
+
+}
---
+++ Seminar10/example1_sem10/sq-ch14-ex1/src/main/java/com/example/model/Account.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+@Data
+public class Account {
+
+  @Id
+  private long id;
+
+  private String name;
+  private BigDecimal amount;
+
+}
